ad0b1df4dc2284fa9d62cd6a8081ac977e009bca
András Horváth <horvath.andras@prevaudit.hu>
2021-10-18 14:55:04 +0200

    The Registration class has been created.

---
+++ training-solutions/training-solutions/src/main/java/classstructureio/Registration.java
@@ -0,0 +1,25 @@
+package classstructureio;
+
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
+public class Registration {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name;
+        String emailAddress;
+
+        System.out.println("Please, enter your name!");
+        name = scanner.nextLine();
+
+        System.out.println("Enter your email address!");
+        emailAddress = scanner.nextLine();
+
+        System.out.println("You have successfully registered.");
+        System.out.println("Your name: " + name);
+        System.out.println("Your email address: " + emailAddress);
+    }
+}

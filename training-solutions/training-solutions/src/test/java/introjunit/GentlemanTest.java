e9534f0468b708c120ea334f962fee2bfb6cd7a8
András Horváth <horvath.andras@prevaudit.hu>
2021-11-13 11:41:42 +0100

    introjunit imports and tests have been commented out because of change to JUnit 5

--- training-solutions/training-solutions/src/test/java/introjunit/GentlemanTest.java
+++ training-solutions/training-solutions/src/test/java/introjunit/GentlemanTest.java
@@ -1,48 +1,50 @@
 package introjunit;
 
-
-import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+////JUnit-4-specific imports have been commented out because of change to JUnit 5 in pom.xml file.
+//
+//import org.junit.Test;
+//
+//import static org.hamcrest.CoreMatchers.equalTo;
+//import static org.junit.Assert.assertThat;
 
 public class GentlemanTest {
-
-    // Given
-    Gentleman gentleman = new Gentleman();
-
-    // --- Első teszt implementálása ------------------------------------------
-
-    @Test
-    public void testSayHello() {
-        // When
-        String gentlemanSentence = gentleman.sayHello("John Doe");
-        // Then
-        assertThat(gentlemanSentence, equalTo("Hello, John Doe!"/* + "!"*/));
-        // The part commented out hase been used for testing JUnit.
-    }
-
-    // --- Hibás teszt --------------------------------------------------------
-
-    /*
-    Programhiba:
-    java.lang.AssertionError:
-    Actual   :Hello, John Doe!!
-    Expected :Hello, John Doe!
-
-    Teszthiba:
-    java.lang.AssertionError:
-    Expected :Hello, John Doe!!
-    Actual   :Hello, John Doe!
-    */
-
-    // --- Tesztlefedettség mérése --------------------------------------------
-
-    @Test
-    public void testSayHelloForNull() {
-        // When
-        String gentlemanSentenceNull = gentleman.sayHello(null);
-        // Then
-        assertThat(gentlemanSentenceNull, equalTo("Hello, Anonymous!"));
-    }
+//    // Class with JUnit-4-specific tests has been commented out because of change to JUnit 5 in pom.xml file.
+//
+//    // Given
+//    Gentleman gentleman = new Gentleman();
+//
+//    // --- Első teszt implementálása ------------------------------------------
+//
+//    @Test
+//    public void testSayHello() {
+//        // When
+//        String gentlemanSentence = gentleman.sayHello("John Doe");
+//        // Then
+//        assertThat(gentlemanSentence, equalTo("Hello, John Doe!"/* + "!"*/));
+//        // The part commented out has been used for testing JUnit.
+//    }
+//
+//    // --- Hibás teszt --------------------------------------------------------
+//
+//    /*
+//    Programhiba:
+//    java.lang.AssertionError:
+//    Actual   :Hello, John Doe!!
+//    Expected :Hello, John Doe!
+//
+//    Teszthiba:
+//    java.lang.AssertionError:
+//    Expected :Hello, John Doe!!
+//    Actual   :Hello, John Doe!
+//    */
+//
+//    // --- Tesztlefedettség mérése --------------------------------------------
+//
+//    @Test
+//    public void testSayHelloForNull() {
+//        // When
+//        String gentlemanSentenceNull = gentleman.sayHello(null);
+//        // Then
+//        assertThat(gentlemanSentenceNull, equalTo("Hello, Anonymous!"));
+//    }
 }
